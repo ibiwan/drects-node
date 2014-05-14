0f2e537be1911a9347d2a41579f6b71453696d07
Jeremy Kent <jeremy@optimozo.com>
2014-05-13 21:36:22 -0700

    address more code review suggestions

--- rects_s.js
+++ rects_s.js
@@ -1,48 +1,52 @@
 #!/usr/local/bin/node
 
 var fs         = require('fs');
-// npm install express body-parser
+
+// npm install express body-parser morgan
+var morgan     = require('morgan');
 var express    = require('express');
 var bodyParser = require('body-parser');
 
+handlers = {
+  'mainpage' : function mainpage(req, res)
+      {
+          console.log("mainpage()");
+          res.sendfile(__dirname + '/rects_c.html');
+      },
+  'getdoc' : function getdoc(req, res)
+      {
+          console.log("getdoc()");
+          res.sendfile(__dirname + "/dnd.json");
+      },
+  'savedoc' : function savedoc(req, res)
+      {
+          console.log("savedoc()");
+
+          var filedata = req.body.file;
+          filedata = JSON.stringify(JSON.parse(filedata), null, '  ');
+
+          var filename = __dirname + "/saved.json";
+          fs.writeFile(filename, filedata, {}, function(err){
+              if( err )
+              {
+                  console.log("error saving:", err);
+                  res.json(500, {'success':false,'error':err});
+              } else {
+                  res.json(200, {'success':true});
+              }
+          });
+      }
+};
+
 var port = 1338;
 var app = express()
-    .get('/',                       mainpage)
-    .get('/getdoc',                 getdoc)
-    .post('/savedoc', bodyParser(), savedoc)
+    .use(morgan('dev'))
+    .get('/',                       handlers.mainpage)
+    .get('/getdoc',                 handlers.getdoc)
+    .post('/savedoc', bodyParser(), handlers.savedoc)
     .get('/favicon.ico', function(req, res){
         res.send(404, 'No favicon');
     })
     .listen(port);
 console.log("listening on ", port);
 
-function mainpage(req, res)
-{
-    console.log("mainpage()");
-    res.sendfile('rects_c.html');
-}
-
-function getdoc(req, res)
-{
-    console.log("getdoc()");
-    res.sendfile("dnd.json");
-}
-
-function savedoc(req, res)
-{
-    console.log("savedoc()");
-
-    var filedata = req.body.file;
-    filedata = JSON.stringify(JSON.parse(filedata), null, '  ');
-
-    var filename = "saved.json";
-    fs.writeFile(filename, filedata, {}, function(err){
-        if( err )
-        {
-            console.log("error saving:", err);
-            res.json(500, {'success':false,'error':err});
-        } else {
-            res.json(200, {'success':true});
-        }
-    });
-}
