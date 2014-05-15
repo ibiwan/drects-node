4586c97462967c44bf5ba32dff340e2e4c8e351d
Jeremy Kent <jeremy@optimozo.com>
2014-05-14 22:03:45 -0700

    start your parsing engines!

--- parser.js
+++ parser.js
@@ -2,10 +2,12 @@ formula_candidates = [
     '=sum(3)',
     '=this.parent',
     '=count(7,this.parent.children[*])',
+    '=this.parent.children[*]',
     'sum(3)',
     '=57',
     '=-23',
     '=1.57',
+    '=null',
 ];
 
 function startswith(bigstring, prefix)
@@ -28,33 +30,114 @@ function startswithany(bigstring, prefixes)
 }
 
 productions = {
+    'scalar_subpath' : {
+        'prefixes' : ['.'],
+        'parse'    : function(v) {
+            console.log("scalar_subpath:", v);
+            return true;
+        },
+    },
+    'unary_call' : {
+        'prefixes' : ['neg', 'abs'],
+        'parse'    : function(v) {
+            console.log("unary_call:", v);
+            return true;
+        },
+    },
+    'binary_call' : {
+        'prefixes' : ['add', 'sub', 'mul', 'div', 'mod'],
+        'parse'    : function(v) {
+            console.log("binary_call:", v);
+            return true;
+        },
+    },
+    'mixed_call' : {
+        'prefixes' : ['count'], // <<FIXME>> need 'find' but that's an aggregate-RETURN call
+        'parse'    : function(v) {
+            console.log("mixed_call:", v);
+            return true;
+        },
+    },
+    'aggregate_call' : {
+        'prefixes' : ['sum', 'prod', 'min', 'max', 'mean'],
+        'parse'    : function(v) {
+            console.log("aggregate_call:", v);
+            return true;
+        },
+    },
     'number' : {
         'prefixes' : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'],
-        'parse'   : function(v) {return true;},
+        'parse'    : function(v) {
+            console.log("number:", v);
+            return true;
+        },
     },
     'string' : {
         'prefixes' : ["'"],
-        'parse'   : function(v) {return true;},
+        'parse'    : function(v) {
+            console.log("string:", v);
+            return true;
+        },
     },
     'bool' : {
         'prefixes' : ['true', 'false'],
-        'parse'   : function(v) {return true;},
+        'parse'    : function(v) {
+            console.log("bool:", v);
+            return true;
+        },
     },
     'null' : {
         'prefixes' : ['null'],
-        'parse'   : function(v) {return true;},
+        'parse'    : function(v) {
+            console.log("null:", v);
+            // <<FIXME>> do something with null
+            return true;
+        },
     },
     'scalar_path' : {
-        'prefixes' : ['root', 'this'],
-        'parse'   : function(v) {return true;},
+        'prefixes' : scalar_path_prefixes = ['root', 'this'],
+        'parse'    : function(v) {
+            console.log("scalar_path:", v);
+            var prefix = startswithany(v, scalar_path_prefixes);
+            var remainder = stringafter(v, prefix);
+            if( prefix === false )
+            {
+                console.log("scalar path must start with root or this");
+                return false;
+            }
+            console.log("starting element:", prefix);
+            // <<FIXME>> do something about chosen prefix
+
+            // check for subpath and parse if present
+            var prod = productions['scalar_subpath'];
+            if( startswithany(remainder, prod.prefixes) )
+            {
+                return prod.parse(remainder);
+            } else {
+                console.log("no subpath");
+            }
+            return true;
+        },
     },
     'function_call' : {
         'prefixes' : ['neg', 'abs', 'add', 'sub', 'mul', 'div', 'mod', 'count', 'find', 'sum', 'prod', 'min', 'max', 'mean'],
-        'parse'   : function(v) {return true;},
+        'parse'    : function(v) {
+            console.log("function_call:", v);
+            var options = ['unary_call','binary_call','mixed_call','aggregate_call'];
+            for(var i = 0; i < options.length; i++)
+            {
+                var option = productions[options[i]];
+                if( startswithany( v, option.prefixes ) )
+                    return option.parse(v);
+            }
+            console.log("function call must start with a function keyword");
+            return false;
+        },
     },
     'scalar' : {
         'prefixes' : [''],
-        'parse'   : function(v) {
+        'parse'    : function(v) {
+            console.log("scalar:", v);
             var options = ['number', 'string', 'bool', 'null', 'scalar_path', 'function_call'];
             for(var i = 0; i < options.length; i++)
             {
@@ -69,6 +152,7 @@ productions = {
     'formula' : {
         'prefixes' : ['='],
         'parse'    : function(v) {
+            console.log("formula:", v);
             var init = '=';
             if( startswith(v, init) )
             {
